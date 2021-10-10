8bd84d98164a8b83d50e058d702ec31c151246e0
Tarık <50327424+tgulyuva@users.noreply.github.com>
2021-10-10 17:12:13 +0300

    update cdc test

--- cdc/provider_test.go
+++ cdc/provider_test.go
@@ -2,18 +2,20 @@ package cdc
 
 import (
 	"fmt"
+	"net/http"
 	"testing"
 
 	"github.com/pact-foundation/pact-go/dsl"
 	"github.com/pact-foundation/pact-go/types"
+	"github.com/tgulyuva/todo-backend"
 )
 
-func TestProvider_TodoGetPost(t *testing.T) {
+func TestProvider_TodoPost(t *testing.T) {
 	pact := &dsl.Pact{
 		Provider: "todo-list-backend",
 	}
 
-	port := 3000
+	port := 3001
 
 	pact.VerifyProvider(t, types.VerifyRequest{
 		ProviderBaseURL:            fmt.Sprintf("http://127.0.0.1:%d", port),
@@ -24,24 +26,17 @@ func TestProvider_TodoGetPost(t *testing.T) {
 
 		BeforeEach: func() error {
 
-			//	app := main.EndPoints()
-
-			//go app.Listen(fmt.Sprint(":", port))
-			//go http.ListenAndServe(fmt.Sprint("localhost:", port), app)
+			app := main.EndPoints()
+			go http.ListenAndServe(fmt.Sprint("localhost:", port), app)
 
 			return nil
 		},
 		StateHandlers: types.StateHandlers{
-			"there is 3 todo": func() error {
-				//err := repository.AddTodoRepository(&TodoModel{ID: "1", Todo: "test 1"})
-				//err = repository.AddTodoRepository(&TodoModel{ID: "2", Todo: "test 2"})
-				//err = repository.AddTodoRepository(&TodoModel{ID: "3", Todo: "test 3"})
 
-				return nil
-			},
-			"there is 1 todo": func() error {
-				//err := repository.AddTodoRepository(&TodoModel{ID: "4", Todo: "Buy the milk"})
-				return nil
+			"there is task": func() error {
+				// msg := models.ResponseModel{Success: false}
+				// return msg
+				return nil // hata almak için nil dönüş yapıldı.
 			},
 		}},
 	)
